6912f0cd9a2acbefbf0f34ebc8547ffa61ce6a30
suhang1994 <suhang1994@gmail.com>
2016-12-20 15:56:29 +0800

    upgrade to 1.10

--- dog/dog/urls.py
+++ dog/dog/urls.py
@@ -1,11 +1,12 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
+from dog import views
+import paper
 
-urlpatterns = patterns(
-    '',
-    url(r'^$', 'dog.views.index', name='index'),
-    url(r'^index/$', 'dog.views.index', name='index'),
-    url(r'^paper/$', 'paper.views.paper_list'),
-    url(r'^author/$', 'paper.views.author_list'),
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^index/$', views.index, name='index'),
+    # url(r'^paper/$', paper.views.paper_list),
+    # url(r'^author/$', paper.views.author_list),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
